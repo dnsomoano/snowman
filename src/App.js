42488c58f279ac6738b81859a6a68d7e2fe9414c
Daniel N Somoano <dnsomoano@gmail.com>
2018-08-28 20:10:28 -0400

    Added assets folder, but could not resolve first image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import LetterButton from "./Components/LetterButton";
-// import words from "./Data/words.json";
+// TODO
+import words from "./Data/words.json";
+import snowman0 from "./assets.step_0.png";
+import snowman1 from "./assets.step_1.png";
+import snowman2 from "./assets.step_2.png";
+import snowman3 from "./assets.step_3.png";
+import snowman4 from "./assets.step_4.png";
+import snowman5 from "./assets.step_5.png";
+import snowman6 from "./assets.step_6.png";
+import snowman7 from "./assets.step_7.png";
 
 const ALPHABET = "abcdefghijklmnopqrstuvwxyz".split("");
 
@@ -9,10 +18,15 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pickedLetters: []
+      pickedLetters: [],
+      // test key values
+      secret: [],
+      // picked: []
+      snowman: snowman0
     };
   }
 
+  // Adding letter to array for picked
   addLetterToArray = letter => {
     console.log("Works here", letter);
     const _newPickedLetter = this.state.pickedLetters.slice();
@@ -22,9 +36,34 @@ class App extends Component {
     });
   };
 
+  // generate secret word randomly
+  // TODO
+  generateSecret = () => {
+    const randomWord = Math.floor(Math.random() * words.length);
+    const underscores = "_".repeat(words[randomWord].length).split("");
+    this.setState({
+      secret: words[randomWord]
+    });
+    return underscores;
+  };
+
+  // Compare every letter of chosen to every letter of secret
+  compareLettersToWord = event => {
+    const _secretWord = this.state.secret;
+    // dbg
+    console.log(typeof this.state.pickedLetters);
+    console.log(this.state.pickedLetters);
+    for (let index = 0; index < _secretWord.length; index++) {
+      console.log("checking", _secretWord[index]);
+      if (this.state.pickedLetters.includes(_secretWord[index])) {
+        console.log("matched", _secretWord[index]);
+
+        // return <div>{index}</div>;
+      }
+    }
+  };
+
   render() {
-    // const randomWord = Math.floor(Math.random(words[Math.floor] * 2));
-    // console.log(randomWord);
     return (
       <div className="App">
         {ALPHABET.map((letter, i) => {
@@ -37,7 +76,19 @@ class App extends Component {
             />
           );
         })}
-
+        <section className="image-container">
+          <img src={this.state.snowman} />
+        </section>
+        {/* Test inputs */}
+        <section>
+          <button onClick={this.generateSecret}>New Game</button>
+          {/* key functionality */}
+          <section>
+            <button onClick={this.compareLettersToWord}>Compare Letters</button>
+          </section>
+        </section>
+        {/*  */}
+        {/* <section></section> */}
         <h3>Letters picked</h3>
         {this.state.pickedLetters.map((letter, i) => {
           return <div key={i}>{letter}</div>;
